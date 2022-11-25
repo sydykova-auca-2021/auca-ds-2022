bdf399313cc083a5b7eeb88a8d83266b19db20df
Aziza Sydykova <sydykova_az@auca.kg>
2022-11-25 04:38:24 +0100

    finished constructor with a string parameter

--- project01/BigInt/BigInt.hpp
+++ project01/BigInt/BigInt.hpp
@@ -1,44 +1,151 @@
 #pragma once
 
-#include <vector>
+#include <iostream>
 #include <string>
-#include <stdexcept>
+#include <sstream>
+#include <vector>
+#include <algorithm>
 
 
 class BigInt
 {
     friend std::ostream &operator<<(std::ostream &out, const BigInt &x);
+    friend std::istream &operator>>(std::istream &inp, BigInt &x);
 
     std::vector<int> mDigits;
     bool mIsNegative;
 
+    static BigInt addAbsValues(const BigInt &x, const BigInt &y)
+    {
+        auto itX = x.mDigits.rbegin();
+        auto itY = y.mDigits.rbegin();
+
+        BigInt z;
+        z.mDigits.resize(std::max(x.mDigits.size(), y.mDigits.size()) + 1);
+        auto itZ = z.mDigits.rbegin();
+        int carry = 0;
+
+        while (itX != x.mDigits.rend() || itY != y.mDigits.rend())
+        {
+            int s = carry;
+
+            if (itX != x.mDigits.rend())
+            {
+                s += *itX;
+                ++itX;
+            }
+            if (itY != y.mDigits.rend())
+            {
+                s += *itY;
+                ++itY;
+            }
+
+            *itZ = s % 10;
+            carry = (s > 9) ? 1 : 0;
+            ++itZ;
+        }
+
+        if (carry != 0)
+        {
+            *itZ = carry;
+        }
+        if (z.mDigits.size() > 1 && z.mDigits.front() == 0)
+        {
+            z.mDigits.erase(z.mDigits.begin());
+        }
+        return z;
+    }
+
 public:
     BigInt()
         : mIsNegative(false)
+    {
+        mDigits.push_back(0);
+    }
+    BigInt(const std::string &s)
+        : mIsNegative(false)
+    {
+        if (s.empty())
         {
-            mDigits.push_back(0);
+            throw std::runtime_error("Cannot be empty");
+        }
+        if (s[0] == '-')
+        {
+            mIsNegative = true;
         }
 
-        BigInt(const::std::string &s)
-        : mIsNegative(false)
+        for (int i = 0; i < s.size(); ++i)
         {
-            for(auto d : s)
+            if (i == 0 && (s[i] == '-' || s[i] == '+'))
             {
-                mDigits.push_back(d - '0');
+                continue;
+            }
+            if (isdigit(s[i]))
+            {
+                mDigits.push_back(s[i] - '0');
+            }
+            else
+            {
+                throw std::runtime_error("Invalid number");
             }
         }
+    }
+
+    BigInt(const int &num)
+        : mIsNegative(false)
+    {
+        if (num < 0)
+        {
+            mIsNegative = true;
+        }
+        std::string s = std::to_string(num);
+        for (auto d : s)
+        {
+            mDigits.push_back(d - '0');
+        }
+    }
+    
+    BigInt(long long x)
+        : BigInt(std::to_string(x))
+    {      
+    }
 };
 
 inline std::ostream &operator<<(std::ostream &out, const BigInt &x)
 {
-    if(x.mIsNegative)
+    if (x.mIsNegative)
     {
         out << "-";
     }
-    for(auto d : x.mDigits)
+    for (auto digit : x.mDigits)
     {
-        out << d;
+        out << digit;
     }
-
     return out;
+}
+
+inline std::istream &operator>>(std::istream &inp, BigInt &x)
+{
+    char ch;
+    if(!(inp >> ch))
+    {
+        return inp;
+    }
+    if(!(std::isdigit(ch) || ch == '+' || '-'))
+    {
+        inp.putback(ch);
+        inp.setstate(std::ios_base::failbit);
+        return inp; 
+    }
+    std::string s;
+    if(std::isdigit(ch))
+    {
+        s += ch;
+    }
+    while (inp.get(ch) && std::isdigit(ch))
+    {
+        s += ch;
+    }
+    x = BigInt(s);
+    return inp;
 }
--- project01/BigInt/main.cpp
+++ project01/BigInt/main.cpp
@@ -1,10 +1,12 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "../../doctest/doctest.h"
-
 #include <sstream>
+#include <string>
+#include <stdexcept>
+#include <ctype.h>
+#include <vector>
 
 #include "BigInt.hpp"
-
 using namespace std;
 
 TEST_CASE("Default constructor")
@@ -14,24 +16,73 @@ TEST_CASE("Default constructor")
     sout << x;
     REQUIRE(sout.str() == "0");
 
-    vector<BigInt> v(10);
+    vector<BigInt> v(5);
     for (const auto &e : v)
     {
         ostringstream sout2;
         sout2 << e;
         REQUIRE(sout2.str() == "0");
     }
-};
-
+}
 TEST_CASE("Constructor with a string parameter")
 {
     ostringstream sout;
-
-    SUBCASE("Just long correct positive number")
+    SUBCASE("positive number")
     {
-        BigInt x("123123123");
+        BigInt x("122344556");
         sout << x;
-        REQUIRE(sout.str() == "123123123");
+        REQUIRE(sout.str() == "122344556");
+    }
+    SUBCASE("negative number")
+    {
+        BigInt x("-1234");
+        sout << x;
+        REQUIRE(sout.str() == "-1234");
+    }
+    SUBCASE("number with sign")
+    {
+        BigInt x("-123");
+        sout << x;
+        REQUIRE(sout.str() == "-123");
+
+        sout.str("");
+        BigInt y("+123");
+        sout << y;
+        REQUIRE(sout.str() == "123");
+
+        // sout.str("");
+        // BigInt z("-0");
+        // sout << z;
+        // REQUIRE(sout.str() == "0");
     }
 
-};
+    // SUBCASE("leading zeroes")
+    // {
+    //     BigInt x("-0000123");
+    //     sout << x;
+    //     REQUIRE(sout.str() == "-123");
+
+    //     sout.str("");
+    //     BigInt y("+00000000");
+    //     sout << y;
+    //     REQUIRE(sout.str() == "0");
+    // }
+    // SUBCASE("just sign")
+    // {
+    //     REQUIRE_THROWS_AS(BigInt("+"), runtime_error);
+    //     REQUIRE_THROWS_AS(BigInt("-"), runtime_error);
+    // }
+    SUBCASE("empty space")
+    {
+        REQUIRE_THROWS_AS(BigInt(""), runtime_error);
+        REQUIRE_THROWS_WITH(BigInt(""), "Cannot be empty");
+    }
+    SUBCASE("String containning letters")
+    {
+        REQUIRE_THROWS_AS(BigInt("12hjgjhgyj3"), runtime_error);
+    }
+    SUBCASE("Invalid string with spaces")
+    {
+        REQUIRE_THROWS_AS(BigInt("123 456"), runtime_error);
+    }
+}
