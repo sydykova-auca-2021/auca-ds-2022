16a870146a78d620c16b3c7625006d038da98e7b
Aziza Sydykova <sydykova_az@auca.kg>
2022-12-09 08:46:25 +0100

    project02 problem05 is finished

--- project02/p05/main.cpp
+++ project02/p05/main.cpp
@@ -1,57 +1,67 @@
 #include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
 
 template <typename C>
 int sz(const C &c) { return static_cast<int>(c.size()); }
 
 using namespace std;
 
-struct Skill
+struct PairSecond
 {
-    int conSalary;
-    string conSkill;
-    
-    Skill(string skill, int salary)
-        : conSkill(skill), conSalary(salary)
+    int mAscii;
+    int mFreq;
+    PairSecond(int ascii, int freq)
+        : mAscii(ascii), mFreq(freq)
     {
     }
+    bool operator==(const int p) const { return this->mAscii == p; }
 };
+
+int freqs[200];
 
 int main()
 {
-    iostream::sync_with_stdio(false);
-
-    int nOfSkills, nOfTexts;
-    cin >> nOfSkills;
-    cin >> nOfTexts;
-    vector<Skill> skills;
-
-    for (int i = 0; i < nOfSkills; i++)
+    string line;
+    int z = 1;
+    int asValue;
+    while (getline(cin, line))
     {
-        string skill;
-        int salary;
-        cin >> skill >> salary;
-        skills.push_back(Skill(skill, salary));
-    }
+        iostream::sync_with_stdio(false);
 
-    sort(begin(skills), end(skills), [](Skill skill1, Skill skill2)
-        { return skill1.conSkill < skill2.conSkill; });
+        if (z == 0)
+            cout << endl;
+        vector<PairSecond> pairs;
 
-    int result = 0;
-    string skill;
-
-    while (cin >> skill)
-    {
-        if (skill == ".")
+        for (size_t i = 0; i < line.length(); ++i)
         {
-            cout << result << endl;
-            result = 0;
+            char c = line[i];
+            asValue = c;
+            auto aschiIt = find(pairs.begin(), pairs.end(), asValue);
+            if (aschiIt == pairs.end())
+            {
+                pairs.push_back(PairSecond(asValue, 1));
+            }
+            else
+            {
+                aschiIt->mFreq++;
+            }
         }
 
-        auto it = lower_bound(begin(skills), end(skills), skill, [](Skill a, string b)
-            { return a.conSkill < b; });
-        if (it != skills.end() && (it->conSkill == skill))
+        sort(begin(pairs), end(pairs), [](const PairSecond &p1, const PairSecond &p2)
+        {  
+            if (p1.mFreq < p2.mFreq)
+                return true;
+            else if(p1.mFreq == p2.mFreq)
+                return p1.mAscii > p2.mAscii;
+            else 
+                return false; 
+        });
+        for (auto i = pairs.begin(); i != pairs.end(); i++)
         {
-            result += (it->conSalary);
+            cout << i->mAscii << " " << i->mFreq << endl;
         }
+        z = 0;
     }
 }
